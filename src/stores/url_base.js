21e0a34f76fcd86aa15a58bcb953102c6facc6c1
Federico <fedeprenollio@gmail.com>
2024-04-17 22:13:20 -0300

    cambios

--- src/stores/url_base.js
+++ src/stores/url_base.js
@@ -1,2 +1,4 @@
-export const BACKEND_URL_BASE = import.meta.env.VITE_BACKEND_URL || 'http://localhost:1234';
+// export const BACKEND_URL_BASE = import.meta.env.VITE_BACKEND_URL || 'http://localhost:1234';
+export const BACKEND_URL_BASE = 'http://localhost:1234';
+
 console.log("NO ANDA", import.meta.env.VITE_BACKEND_URL  )
