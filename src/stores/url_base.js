f641525b97f0b698af56d9717c5f9e68e992d980
Federico <fedeprenollio@gmail.com>
2024-04-17 22:28:47 -0300

    update urlbase

--- src/stores/url_base.js
+++ src/stores/url_base.js
@@ -1,4 +1,9 @@
-// export const BACKEND_URL_BASE = import.meta.env.VITE_BACKEND_URL || 'http://localhost:1234';
-export const BACKEND_URL_BASE = 'http://localhost:1234';
+export const BACKEND_URL_BASE = import.meta.env.VITE_BACKEND_URL || 'http://localhost:1234';
+// export const BACKEND_URL_BASE = 'http://localhost:1234';
 
 console.log("NO ANDA", import.meta.env.VITE_BACKEND_URL  )
+
+
+// export const BACKEND_URL_BASE = import.meta.env.VITE_USE_LOCAL_BACKEND === true
+//   ? import.meta.env.VITE_BACKEND_URL_LOCAL
+//   : import.meta.env.VITE_BACKEND_URL;
