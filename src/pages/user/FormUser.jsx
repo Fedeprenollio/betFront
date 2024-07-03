a9ccfce8914970331e73d96a066e9ee1e33d4045
Federico <fedeprenollio@gmail.com>
2024-07-03 00:14:24 -0300

    added styles in login

--- src/pages/user/FormUser.jsx
+++ src/pages/user/FormUser.jsx
@@ -1,7 +1,6 @@
-/* eslint-disable react/prop-types */
 import { Formik, Form, Field } from "formik";
 import * as yup from "yup";
-import { Button, TextField, Typography } from "@mui/material";
+import { Button, TextField, Typography, Grid, Box } from "@mui/material";
 import { useBoundStore } from "../../stores";
 import { useEffect, useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom"; // Importa useNavigate de react-router-dom
@@ -17,21 +16,20 @@ const FormUser = ({ action }) => {
     registerUser,
     error,
     isAuthenticated,
-    setToken
+    setToken,
   } = useBoundStore((state) => state);
   const [loginError, setLoginError] = useState(null);
   const [user, setUser] = useState(null);
   const navigate = useNavigate(); // Inicializa navigate usando useNavigate
 
   useEffect(() => {
-   const loggedUserJSON = window.localStorage.getItem("loggedUser")
-   if(loggedUserJSON){
-    const user = JSON.parse(loggedUserJSON)
-    setUser(user)
-    setToken(user?.token)
-   }
-  }, [])
-  
+    const loggedUserJSON = window.localStorage.getItem("loggedUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      setToken(user?.token);
+    }
+  }, []);
 
   const handleSubmit = async (values, { resetForm }) => {
     // alert(JSON.stringify(values, null, 2));
@@ -46,11 +44,10 @@ const FormUser = ({ action }) => {
     } else if (action === "login") {
       try {
         const loginResponse = await loginUser({ values });
-        window.localStorage.setItem("loggedUser", JSON.stringify(loginResponse))
-
+        window.localStorage.setItem("loggedUser", JSON.stringify(loginResponse));
 
         setUser(loginResponse);
-        setToken(loginResponse.token)
+        setToken(loginResponse.token);
         console.log("loginResponse", loginResponse);
         if (loginResponse?.status === "ok") {
           // Redireccionar a la página de administrador si el inicio de sesión es exitoso
@@ -61,9 +58,7 @@ const FormUser = ({ action }) => {
         }
       } catch (error) {
         console.log("EROR LOGIN", error);
-        setLoginError(
-          "Error en credenciales"
-        );
+        setLoginError("Error en credenciales");
       }
     }
     resetForm();
@@ -72,56 +67,96 @@ const FormUser = ({ action }) => {
   if (isAuthenticated) {
     return <Navigate to="/" />;
   }
-console.log("ERROR EN LOGIN", loginError, user)
+  console.log("ERROR EN LOGIN", loginError, user);
+
   return (
-    <Formik
-      initialValues={{
-        username: "",
-        password: "",
-      }}
-      validationSchema={validationSchema}
-      onSubmit={handleSubmit}
-    >
-      {({ errors, touched }) => (
-        <Form>
+    <Grid container sx={{ height: "100vh" }}>
+      <Grid
+        item
+        xs={12}
+        md={8}
+        sx={{
+          display: { md: "block" },
+          backgroundImage: 'url("https://media.istockphoto.com/id/1610631888/es/foto/ni%C3%B1o-jugando-al-f%C3%BAtbol-en-el-parque-local.jpg?s=1024x1024&w=is&k=20&c=327GrWMs82CJCYc53lW_lREejTuJbKz0kWldcrOj7V4=")',
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <Grid
+        item
+        xs={12}
+        md={4}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          p:2,
+        }}
+      >
+        <Box
+          sx={{
+            width: { xs: "50%", md: "75%" },
+            textAlign: "center",
+            mb: 2,
+          }}
+        >
+          <img
+            src="https://cdn.pixabay.com/photo/2013/07/13/10/51/football-157930_1280.png"
+            alt="Logotipo"
+            style={{ maxWidth: "100%", marginBottom: 16 }}
+          />
           <Typography variant="h5" gutterBottom>
             {action === "register" ? "Crea una cuenta" : "Logueate"}
           </Typography>
-          <Field
-            as={TextField}
-            fullWidth
-            id="username"
-            name="username"
-            label="Usuario"
-            variant="outlined"
-            margin="dense"
-            autoComplete="off "
-            error={touched.username && Boolean(errors.username)}
-            helperText={touched.username && errors.username}
-          />
-          <Field
-            as={TextField}
-            fullWidth
-            id="password"
-            name="password"
-            label="Contraseña"
-            type="password"
-            variant="outlined"
-            margin="dense"
-            error={touched.password && Boolean(errors.password)}
-            helperText={touched.password && errors.password}
-          />
-          {loginError && (
-            <Typography variant="body2" color="error">
-              {loginError}
-            </Typography>
+        </Box>
+        <Formik
+          initialValues={{
+            username: "",
+            password: "",
+          }}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ errors, touched }) => (
+            <Form style={{ width: "100%" }}>
+              <Field
+                as={TextField}
+                fullWidth
+                id="username"
+                name="username"
+                label="Usuario"
+                variant="outlined"
+                margin="dense"
+                autoComplete="off"
+                error={touched.username && Boolean(errors.username)}
+                helperText={touched.username && errors.username}
+              />
+              <Field
+                as={TextField}
+                fullWidth
+                id="password"
+                name="password"
+                label="Contraseña"
+                type="password"
+                variant="outlined"
+                margin="dense"
+                error={touched.password && Boolean(errors.password)}
+                helperText={touched.password && errors.password}
+              />
+              {loginError && (
+                <Typography variant="body2" color="error">
+                  {loginError}
+                </Typography>
+              )}
+              <Button type="submit" variant="contained" color="primary" fullWidth sx={{ mt: 2 }}>
+                Iniciar sesión
+              </Button>
+            </Form>
           )}
-          <Button type="submit" variant="contained" color="primary">
-            Iniciar sesión
-          </Button>
-        </Form>
-      )}
-    </Formik>
+        </Formik>
+      </Grid>
+    </Grid>
   );
 };
 
