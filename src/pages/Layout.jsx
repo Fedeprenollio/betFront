b0ced7af56f7a7b554cea36d24fc176672f0ca12
Fedeprenollio <fede.preno@gmail.com>
2024-07-09 15:40:10 -0300

    add GoogleAd

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,12 +1,13 @@
 import Navbar from "./Navbar";
 import RoutesComponent from "../router/RoutesComponents";
+import GoogleAd from "../componts/googleAd/GoogleAd";
 
 
 const Layout = () => {
   return (
     <>
       <Navbar />
-     
+     <GoogleAd/>
       <RoutesComponent />
      
     </>
