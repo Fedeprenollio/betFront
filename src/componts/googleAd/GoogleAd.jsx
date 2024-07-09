6f986a7b98aecf02b151ee098d6a83426894a74c
Fedeprenollio <fede.preno@gmail.com>
2024-07-09 17:28:58 -0300

    add google add

--- src/componts/googleAd/GoogleAd.jsx
+++ src/componts/googleAd/GoogleAd.jsx
@@ -1,33 +1,21 @@
 import React, { useEffect } from 'react';
 
 const GoogleAd = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
-    script.async = true;
-    script.setAttribute('data-ad-client', 'ca-pub-1235260714133990'); // Reemplaza con tu ID de cliente de AdSense
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
-  useEffect(() => {
-    try {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
-    } catch (e) {
-      console.error(e);
+   useEffect(() => {
+    if (window.adsbygoogle) {
+      window.adsbygoogle.push({});
     }
   }, []);
 
   return (
-    <ins className="adsbygoogle"
-         style={{ display: 'block' }}
-         data-ad-client="ca-pub-1235260714133990" // Reemplaza con tu ID de cliente de AdSense
-         data-ad-slot="1234567890" // Reemplaza con tu Ad Slot ID
-         data-ad-format="auto"
-         data-full-width-responsive="true"></ins>
+    <div>
+      <ins className="adsbygoogle"
+        style={{ display: 'block' }}
+        data-ad-client="ca-pub-1235260714133990"
+        data-ad-slot="1234567890"
+        data-ad-format="auto"
+        data-full-width-responsive="true"></ins>
+    </div>
   );
 };
 
