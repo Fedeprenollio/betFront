be08f234c095c2665c9a8d966ea396796de0bcd7
Fedeprenollio <fede.preno@gmail.com>
2024-07-09 15:36:26 -0300

    update adsense

--- src/componts/googleAd/GoogleAd.jsx
+++ src/componts/googleAd/GoogleAd.jsx
@@ -5,7 +5,7 @@ const GoogleAd = () => {
     const script = document.createElement('script');
     script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
     script.async = true;
-    script.setAttribute('data-ad-client', 'YOUR_ADSENSE_CLIENT_ID'); // Reemplaza con tu ID de cliente de AdSense
+    script.setAttribute('data-ad-client', '1031386387'); // Reemplaza con tu ID de cliente de AdSense
     document.body.appendChild(script);
 
     return () => {
@@ -16,8 +16,8 @@ const GoogleAd = () => {
   return (
     <ins className="adsbygoogle"
          style={{ display: 'block' }}
-         data-ad-client="YOUR_ADSENSE_CLIENT_ID" // Reemplaza con tu ID de cliente de AdSense
-         data-ad-slot="YOUR_AD_SLOT_ID" // Reemplaza con tu ID de bloque de anuncios
+         data-ad-client="ca-pub-1235260714133990" // Reemplaza con tu ID de cliente de AdSense
+         data-ad-slot="5326574488" // Reemplaza con tu ID de bloque de anuncios
          data-ad-format="auto"
          data-full-width-responsive="true"></ins>
   );
