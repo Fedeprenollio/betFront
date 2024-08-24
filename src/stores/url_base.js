cf59535f98a47ef6beaca1a7e491259518ff809c
Federico <fedeprenollio@gmail.com>
2024-08-24 00:12:22 -0300

    changed base url to https://bets-b3rn.onrender.com

--- src/stores/url_base.js
+++ src/stores/url_base.js
@@ -1,5 +1,5 @@
-// export const BACKEND_URL_BASE = import.meta.env.VITE_BACKEND_URL || 'http://localhost:1234';
-export const BACKEND_URL_BASE = 'http://localhost:1234';
+export const BACKEND_URL_BASE = import.meta.env.VITE_BACKEND_URL || 'http://localhost:1234';
+// export const BACKEND_URL_BASE = 'http://localhost:1234';
 
 console.log("NO ANDA", import.meta.env.VITE_BACKEND_URL  )
 
