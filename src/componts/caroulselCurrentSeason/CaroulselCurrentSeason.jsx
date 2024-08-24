8fdeedecb5cb5b740f0e158133dfd82bbb603747
Federico <fedeprenollio@gmail.com>
2024-08-24 00:17:10 -0300

    testing finger drag on carousel

--- src/componts/caroulselCurrentSeason/CaroulselCurrentSeason.jsx
+++ src/componts/caroulselCurrentSeason/CaroulselCurrentSeason.jsx
@@ -1,9 +1,117 @@
-// CaroulselCurrentSeason.jsx
+// // CaroulselCurrentSeason.jsx
+// import { Link } from 'react-router-dom';
+// import { useBoundStore } from '../../stores';
+// import { useEffect, useRef } from 'react';
+// import { Box, Typography, List, ListItem, ListItemText, ListItemIcon, IconButton, Avatar, Tooltip } from '@mui/material';
+// import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
+// import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+// import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+// export const CaroulselCurrentSeason = () => {
+//     const { allCurrentSeasons, getAllCurrentSeasons } = useBoundStore((state) => state);
+//     useEffect(() => {
+//         getAllCurrentSeasons();
+//     }, [getAllCurrentSeasons]);
+
+//     const carouselRef = useRef(null);
+
+//     const handleScroll = (direction) => {
+//         const scrollAmount = carouselRef.current.clientWidth;
+//         if (direction === 'left') {
+//             carouselRef.current.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+//         } else {
+//             carouselRef.current.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+//         }
+//     };
+
+//     return (
+//         <Box position="relative" sx={{ display: 'flex', alignItems: 'center' }}>
+//          {allCurrentSeasons.length > 0 &&  
+//          <>
+//           <IconButton
+//                 onClick={() => handleScroll('left')}
+//                 sx={{
+//                     position: 'absolute',
+//                     left: 0,
+//                     zIndex: 1,
+//                     backgroundColor: 'white',
+//                     '&:hover': {
+//                         backgroundColor: 'grey.300',
+//                     },
+//                 }}
+//             >
+//                 <ArrowBackIosIcon />
+//             </IconButton>
+//             <Box
+//                 ref={carouselRef}
+//                 display="flex"
+//                 flexDirection="row"
+//                 flexWrap="nowrap"
+//                 overflow="hidden"
+//                 sx={{ width: '100%', scrollBehavior: 'smooth', padding: '0 40px' }}
+//             >
+//                 <List sx={{ display: 'flex', flexDirection: 'row', padding: 0 }}>
+//                     {allCurrentSeasons?.map(season => (
+//                         <ListItem
+//                             key={season._id}
+//                             component={Link}
+//                             to={`/league/${season._id}/positions`}
+//                             sx={{
+//                                 display: 'inline-flex',
+//                                 alignItems: 'center',
+//                                 marginRight: '16px',
+//                                 paddingX:" 8px" ,
+//                                 textDecoration: 'none',
+//                                 color: 'inherit',
+//                                 '&:hover': {
+//                                     color: 'primary.main',
+//                                 },
+//                             }}
+//                         >
+                         
+//                             {season.league.logo ? 
+//                             <Tooltip title={`${season.league.name}`}>
+
+                           
+//                                 <Avatar
+//                                 alt={season.league.name}
+//                                 src={season.league.logo}
+//                                 // title={season.league.name}
+//                                 sx={{ width: 36, height: 36 }}
+//                             /> 
+//                              </Tooltip>:
+//                             <ListItemText primary={`${season.league.name}`} />
+                                
+//                         }
+
+//                         </ListItem>
+//                     ))}
+//                 </List>
+//             </Box>
+//             <IconButton
+//                 onClick={() => handleScroll('right')}
+//                 sx={{
+//                     position: 'absolute',
+//                     right: 0,
+//                     zIndex: 1,
+//                     backgroundColor: 'white',
+//                     '&:hover': {
+//                         backgroundColor: 'grey.300',
+//                     },
+//                 }}
+//             >
+//                 <ArrowForwardIosIcon />
+//             </IconButton>
+//          </>
+//             }
+//         </Box>
+//     );
+// };
+
 import { Link } from 'react-router-dom';
 import { useBoundStore } from '../../stores';
-import { useEffect, useRef } from 'react';
-import { Box, Typography, List, ListItem, ListItemText, ListItemIcon, IconButton, Avatar, Tooltip } from '@mui/material';
-import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
+import { useEffect, useRef, useState } from 'react';
+import { Box, List, ListItem, Avatar, Tooltip, IconButton, ListItemText } from '@mui/material';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
@@ -14,6 +122,9 @@ export const CaroulselCurrentSeason = () => {
     }, [getAllCurrentSeasons]);
 
     const carouselRef = useRef(null);
+    const [isDragging, setIsDragging] = useState(false);
+    const [startPosition, setStartPosition] = useState(0);
+    const [scrollPosition, setScrollPosition] = useState(0);
 
     const handleScroll = (direction) => {
         const scrollAmount = carouselRef.current.clientWidth;
@@ -24,86 +135,128 @@ export const CaroulselCurrentSeason = () => {
         }
     };
 
+    // Eventos para arrastrar con el mouse (desktop)
+    const handleMouseDown = (e) => {
+        setIsDragging(true);
+        setStartPosition(e.pageX - carouselRef.current.offsetLeft);
+        setScrollPosition(carouselRef.current.scrollLeft);
+    };
+
+    const handleMouseMove = (e) => {
+        if (!isDragging) return;
+        const currentPosition = e.pageX - carouselRef.current.offsetLeft;
+        const distanceMoved = currentPosition - startPosition;
+        carouselRef.current.scrollLeft = scrollPosition - distanceMoved;
+    };
+
+    const handleMouseUp = () => {
+        setIsDragging(false);
+    };
+
+    // Eventos para arrastrar con el dedo (touch)
+    const handleTouchStart = (e) => {
+        setIsDragging(true);
+        setStartPosition(e.touches[0].pageX - carouselRef.current.offsetLeft);
+        setScrollPosition(carouselRef.current.scrollLeft);
+    };
+
+    const handleTouchMove = (e) => {
+        if (!isDragging) return;
+        const currentPosition = e.touches[0].pageX - carouselRef.current.offsetLeft;
+        const distanceMoved = currentPosition - startPosition;
+        carouselRef.current.scrollLeft = scrollPosition - distanceMoved;
+    };
+
+    const handleTouchEnd = () => {
+        setIsDragging(false);
+    };
+
     return (
-        <Box position="relative" sx={{ display: 'flex', alignItems: 'center' }}>
-         {allCurrentSeasons.length > 0 &&  
-         <>
-          <IconButton
-                onClick={() => handleScroll('left')}
-                sx={{
-                    position: 'absolute',
-                    left: 0,
-                    zIndex: 1,
-                    backgroundColor: 'white',
-                    '&:hover': {
-                        backgroundColor: 'grey.300',
-                    },
-                }}
-            >
-                <ArrowBackIosIcon />
-            </IconButton>
-            <Box
-                ref={carouselRef}
-                display="flex"
-                flexDirection="row"
-                flexWrap="nowrap"
-                overflow="hidden"
-                sx={{ width: '100%', scrollBehavior: 'smooth', padding: '0 40px' }}
-            >
-                <List sx={{ display: 'flex', flexDirection: 'row', padding: 0 }}>
-                    {allCurrentSeasons?.map(season => (
-                        <ListItem
-                            key={season._id}
-                            component={Link}
-                            to={`/league/${season._id}/positions`}
-                            sx={{
-                                display: 'inline-flex',
-                                alignItems: 'center',
-                                marginRight: '16px',
-                                paddingX:" 8px" ,
-                                textDecoration: 'none',
-                                color: 'inherit',
-                                '&:hover': {
-                                    color: 'primary.main',
-                                },
-                            }}
-                        >
-                         
-                            {season.league.logo ? 
-                            <Tooltip title={`${season.league.name}`}>
-
-                           
-                                <Avatar
-                                alt={season.league.name}
-                                src={season.league.logo}
-                                // title={season.league.name}
-                                sx={{ width: 36, height: 36 }}
-                            /> 
-                             </Tooltip>:
-                            <ListItemText primary={`${season.league.name}`} />
-                                
-                        }
-
-                        </ListItem>
-                    ))}
-                </List>
-            </Box>
-            <IconButton
-                onClick={() => handleScroll('right')}
-                sx={{
-                    position: 'absolute',
-                    right: 0,
-                    zIndex: 1,
-                    backgroundColor: 'white',
-                    '&:hover': {
-                        backgroundColor: 'grey.300',
-                    },
-                }}
-            >
-                <ArrowForwardIosIcon />
-            </IconButton>
-         </>
+        <Box
+            position="relative"
+            sx={{ display: 'flex', alignItems: 'center' }}
+            onMouseDown={handleMouseDown}
+            onMouseMove={handleMouseMove}
+            onMouseUp={handleMouseUp}
+            onMouseLeave={handleMouseUp} // Por si el usuario suelta el mouse fuera del contenedor
+            onTouchStart={handleTouchStart}
+            onTouchMove={handleTouchMove}
+            onTouchEnd={handleTouchEnd}
+        >
+            {allCurrentSeasons.length > 0 &&
+                <>
+                    <IconButton
+                        onClick={() => handleScroll('left')}
+                        sx={{
+                            position: 'absolute',
+                            left: 0,
+                            zIndex: 1,
+                            backgroundColor: 'white',
+                            '&:hover': {
+                                backgroundColor: 'grey.300',
+                            },
+                        }}
+                    >
+                        <ArrowBackIosIcon />
+                    </IconButton>
+                    <Box
+                        ref={carouselRef}
+                        display="flex"
+                        flexDirection="row"
+                        flexWrap="nowrap"
+                        overflow="hidden"
+                        sx={{ width: '100%', scrollBehavior: 'smooth', padding: '0 40px', cursor: isDragging ? 'grabbing' : 'grab' }}
+                    >
+                        <List sx={{ display: 'flex', flexDirection: 'row', padding: 0 }}>
+                            {allCurrentSeasons?.map((season) => (
+                                <ListItem
+                                    key={season._id}
+                                    component={Link}
+                                    to={`/league/${season._id}/positions`}
+                                    sx={{
+                                        display: 'inline-flex',
+                                        alignItems: 'center',
+                                        marginRight: '16px',
+                                        paddingX: "8px",
+                                        textDecoration: 'none',
+                                        color: 'inherit',
+                                        '&:hover': {
+                                            color: 'primary.main',
+                                        },
+                                    }}
+                                >
+                                    {season.league.logo ?
+                                        <Tooltip title={`${season.league.name}`}>
+                                            <Avatar
+                                                alt={season.league.name}
+                                                src={season.league.logo}
+                                                sx={{ width: 36, height: 36 }}
+                                            />
+                                        </Tooltip>
+                                        :
+                                        <ListItemText primary={`${season.league.name}`} />
+                                    }
+                                </ListItem>
+                            ))}
+                        </List>
+                    </Box>
+                    <IconButton
+                        onClick={() => handleScroll('right')}
+                        sx={{
+                            position: 'absolute',
+                            right: 0,
+                            zIndex: 1,
+                            backgroundColor: 'white',
+                            '&:hover': {
+                                backgroundColor: 'grey.300',
+                            },
+                        }}
+                    >
+                        <ArrowForwardIosIcon />
+                    </IconButton>
+                </>
             }
         </Box>
     );
 };
+
--- src/componts/caroulselLogos/CaroulselLogos.jsx
+++ src/componts/caroulselLogos/CaroulselLogos.jsx
@@ -1,3 +1,114 @@
+// /* eslint-disable react/prop-types */
+// import { useEffect, useState, useRef } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { Avatar, Box, IconButton, Tooltip } from '@mui/material';
+// import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+// import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+// import { useBoundStore } from '../../stores';
+
+// const CarouselLogos = ({ seasonId }) => {
+//     const { getSeasonById, seasonById } = useBoundStore((state) => state);
+
+//     const [teams, setTeams] = useState([]);
+//     const navigate = useNavigate();
+//     const carouselRef = useRef(null);
+
+//     useEffect(() => {
+//         if (seasonId) {
+//             getSeasonById(seasonId);
+//         }
+//     }, [seasonId, getSeasonById]);
+
+//     useEffect(() => {
+//         setTeams(seasonById?.season?.teams || []);
+//     }, [seasonById]);
+
+//     const handleTeamClick = (teamId) => {
+//         navigate(`/teams/${teamId}/statistics`);
+//     };
+
+//     const handleScroll = (direction) => {
+//         const scrollAmount = carouselRef.current.clientWidth;
+//         if (direction === 'left') {
+//             carouselRef.current.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+//         } else {
+//             carouselRef.current.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+//         }
+//     };
+
+//     return (
+//       <>
+//       {teams.length >0 &&
+//              (<Box position="relative" sx={{ display: 'flex', alignItems: 'center' }}>
+//             <IconButton
+//                 onClick={() => handleScroll('left')}
+//                 sx={{
+//                     position: 'absolute',
+//                     left: 0,
+//                     zIndex: 1,
+//                     backgroundColor: 'white',
+//                     '&:hover': {
+//                         backgroundColor: 'grey.300',
+//                     },
+//                 }}
+//             >
+//                 <ArrowBackIosIcon />
+//             </IconButton>
+//             <Box
+//                 ref={carouselRef}
+//                 display="flex"
+//                 flexDirection="row"
+//                 flexWrap="nowrap"
+//                 overflow="hidden"
+//                 sx={{ width: '100%', scrollBehavior: 'smooth', padding: '0 40px' }}
+//             >
+//                 {teams?.map((team) => (
+//                     <Box
+//                         key={team._id}
+//                         m={1} // Margin to add some space between avatars
+//                         onClick={() => handleTeamClick(team._id)}
+//                         sx={{
+//                             cursor: 'pointer',
+//                             transition: 'transform 0.3s',
+//                             '&:hover': {
+//                                 transform: 'scale(1.1)',
+//                             },
+//                         }}
+//                     >
+//                       <Tooltip title={`${team.name}`}>
+
+//                         <Avatar
+//                             alt={team.name}
+//                             src={team.logo}
+//                             // title={team.name}
+//                             sx={{ width: 36, height: 36 }}
+//                         />
+//                         </Tooltip>
+//                     </Box>
+//                 ))}
+//             </Box>
+//             <IconButton
+//                 onClick={() => handleScroll('right')}
+//                 sx={{
+//                     position: 'absolute',
+//                     right: 0,
+//                     zIndex: 1,
+//                     backgroundColor: 'white',
+//                     '&:hover': {
+//                         backgroundColor: 'grey.300',
+//                     },
+//                 }}
+//             >
+//                 <ArrowForwardIosIcon />
+//             </IconButton>
+//         </Box>
+//       ) }
+//         </>
+//     );
+// };
+
+// export default CarouselLogos;
+
 /* eslint-disable react/prop-types */
 import { useEffect, useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -8,10 +119,12 @@ import { useBoundStore } from '../../stores';
 
 const CarouselLogos = ({ seasonId }) => {
     const { getSeasonById, seasonById } = useBoundStore((state) => state);
-
     const [teams, setTeams] = useState([]);
     const navigate = useNavigate();
     const carouselRef = useRef(null);
+    const [isDragging, setIsDragging] = useState(false);
+    const [startPosition, setStartPosition] = useState(0);
+    const [scrollPosition, setScrollPosition] = useState(0);
 
     useEffect(() => {
         if (seasonId) {
@@ -36,75 +149,120 @@ const CarouselLogos = ({ seasonId }) => {
         }
     };
 
+    // Eventos para arrastrar con el mouse (desktop)
+    const handleMouseDown = (e) => {
+        setIsDragging(true);
+        setStartPosition(e.pageX - carouselRef.current.offsetLeft);
+        setScrollPosition(carouselRef.current.scrollLeft);
+    };
+
+    const handleMouseMove = (e) => {
+        if (!isDragging) return;
+        const currentPosition = e.pageX - carouselRef.current.offsetLeft;
+        const distanceMoved = currentPosition - startPosition;
+        carouselRef.current.scrollLeft = scrollPosition - distanceMoved;
+    };
+
+    const handleMouseUp = () => {
+        setIsDragging(false);
+    };
+
+    // Eventos para arrastrar con el dedo (touch)
+    const handleTouchStart = (e) => {
+        setIsDragging(true);
+        setStartPosition(e.touches[0].pageX - carouselRef.current.offsetLeft);
+        setScrollPosition(carouselRef.current.scrollLeft);
+    };
+
+    const handleTouchMove = (e) => {
+        if (!isDragging) return;
+        const currentPosition = e.touches[0].pageX - carouselRef.current.offsetLeft;
+        const distanceMoved = currentPosition - startPosition;
+        carouselRef.current.scrollLeft = scrollPosition - distanceMoved;
+    };
+
+    const handleTouchEnd = () => {
+        setIsDragging(false);
+    };
+
     return (
-      <>
-      {teams.length >0 &&
-             (<Box position="relative" sx={{ display: 'flex', alignItems: 'center' }}>
-            <IconButton
-                onClick={() => handleScroll('left')}
-                sx={{
-                    position: 'absolute',
-                    left: 0,
-                    zIndex: 1,
-                    backgroundColor: 'white',
-                    '&:hover': {
-                        backgroundColor: 'grey.300',
-                    },
-                }}
-            >
-                <ArrowBackIosIcon />
-            </IconButton>
-            <Box
-                ref={carouselRef}
-                display="flex"
-                flexDirection="row"
-                flexWrap="nowrap"
-                overflow="hidden"
-                sx={{ width: '100%', scrollBehavior: 'smooth', padding: '0 40px' }}
-            >
-                {teams?.map((team) => (
-                    <Box
-                        key={team._id}
-                        m={1} // Margin to add some space between avatars
-                        onClick={() => handleTeamClick(team._id)}
+        <>
+            {teams.length > 0 && (
+                <Box
+                    position="relative"
+                    sx={{ display: 'flex', alignItems: 'center' }}
+                    onMouseDown={handleMouseDown}
+                    onMouseMove={handleMouseMove}
+                    onMouseUp={handleMouseUp}
+                    onMouseLeave={handleMouseUp} // Por si el usuario suelta el mouse fuera del contenedor
+                    onTouchStart={handleTouchStart}
+                    onTouchMove={handleTouchMove}
+                    onTouchEnd={handleTouchEnd}
+                >
+                    <IconButton
+                        onClick={() => handleScroll('left')}
                         sx={{
-                            cursor: 'pointer',
-                            transition: 'transform 0.3s',
+                            position: 'absolute',
+                            left: 0,
+                            zIndex: 1,
+                            backgroundColor: 'white',
                             '&:hover': {
-                                transform: 'scale(1.1)',
+                                backgroundColor: 'grey.300',
                             },
                         }}
                     >
-                      <Tooltip title={`${team.name}`}>
-
-                        <Avatar
-                            alt={team.name}
-                            src={team.logo}
-                            // title={team.name}
-                            sx={{ width: 36, height: 36 }}
-                        />
-                        </Tooltip>
+                        <ArrowBackIosIcon />
+                    </IconButton>
+                    <Box
+                        ref={carouselRef}
+                        display="flex"
+                        flexDirection="row"
+                        flexWrap="nowrap"
+                        overflow="hidden"
+                        sx={{ width: '100%', scrollBehavior: 'smooth', padding: '0 40px', cursor: isDragging ? 'grabbing' : 'grab' }}
+                    >
+                        {teams?.map((team) => (
+                            <Box
+                                key={team._id}
+                                m={1} // Margin to add some space between avatars
+                                onClick={() => handleTeamClick(team._id)}
+                                sx={{
+                                    cursor: 'pointer',
+                                    transition: 'transform 0.3s',
+                                    '&:hover': {
+                                        transform: 'scale(1.1)',
+                                    },
+                                }}
+                            >
+                                <Tooltip title={`${team.name}`}>
+                                    <Avatar
+                                        alt={team.name}
+                                        src={team.logo}
+                                        sx={{ width: 36, height: 36 }}
+                                    />
+                                </Tooltip>
+                            </Box>
+                        ))}
                     </Box>
-                ))}
-            </Box>
-            <IconButton
-                onClick={() => handleScroll('right')}
-                sx={{
-                    position: 'absolute',
-                    right: 0,
-                    zIndex: 1,
-                    backgroundColor: 'white',
-                    '&:hover': {
-                        backgroundColor: 'grey.300',
-                    },
-                }}
-            >
-                <ArrowForwardIosIcon />
-            </IconButton>
-        </Box>
-      ) }
+                    <IconButton
+                        onClick={() => handleScroll('right')}
+                        sx={{
+                            position: 'absolute',
+                            right: 0,
+                            zIndex: 1,
+                            backgroundColor: 'white',
+                            '&:hover': {
+                                backgroundColor: 'grey.300',
+                            },
+                        }}
+                    >
+                        <ArrowForwardIosIcon />
+                    </IconButton>
+                </Box>
+            )}
         </>
     );
 };
 
 export default CarouselLogos;
+
